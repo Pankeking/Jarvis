d617b69d156dd76eb49b1d42719871288c37962e
Javier <javiersoap@gmail.com>
2023-08-03 14:15:53 +0200

    class and definitions

--- main.py
+++ main.py
@@ -13,153 +13,168 @@ from helpers import format_date
 # TODO Date / hour command
 # TODO Google commands
 # TODO Structure project / Encapsulate
+# TODO Make program into class and instantiates
 
 
 KILL_PHRASE = "end program"
 
-LISTEN_TIMEOUT = 3
+LISTEN_TIMEOUT = 2
 
 # Initialize text-to-speech and speech-recognizer
 tts_engine = pyttsx3.init()
 recognizer = sr.Recognizer()
+recognizer.pause_threshold = 0.5  # Set a lower pause threshold (in seconds)
+tts_engine.setProperty('rate', 180)
 
-def speak(text):
-    tts_engine.say(text)
-    tts_engine.runAndWait()
+class Jarvis:
+    def __init__(self):
+        # Initialize speech recognition and reduce pause threshold
+        self.recognizer = sr.Recognizer()
+        self.recognizer.pause_threshold = 0.5
+        # Initialize text to speech engine and reduce speed rate
+        self.engine = pyttsx3.init()
+        self.engine.setProperty('rate', 180)
+        # Listening status
+        self.is_awake = False
 
+    def speak(self, text):
+        self.engine.say(text)
+        self.engine.runAndWait()
 
-def jarvis_wake_up():
-    print("Suspended, awaiting")
-    # Listen for starting point
-    with sr.Microphone() as source:
-        recognizer.adjust_for_ambient_noise(source)
-        try:
-            start_command = recognizer.listen(source, timeout=LISTEN_TIMEOUT)
-            recognized_start = recognizer.recognize_google(start_command).lower()
-            print(recognized_start)
-            # Check activation command
-            for phrase in WAKE_UP_PHRASES:
-                if phrase.lower() in recognized_start:
-                    speak("What do you want me to do sir?")
-                    return True
-        except sr.WaitTimeoutError as e:
-            # Timeout ocurred, continue waiting for wake-up phrase
-            pass
-        except sr.UnknownValueError as e:
-            # No recognized speech, continue waiting for wake-up phrase
-            pass
-        except sr.RequestError as e:
-            # Error with the speech recognition service, continue waiting for the wake-up phrase
-            speak(e)
-            return False
-
-def listen_for_commands():
-    is_listening = False
-    # Run the app continuosly
-    while True:
-        if not is_listening:
-            is_listening = jarvis_wake_up()
-            # if KILL_PHRASE.lower() in recognized_start.lower():
-            #   speak("I believe your intentions to be hostile")
-            #   return 0
-        elif is_listening:
+    def jarvis_wake_up(self):
+        # Listen for starting point
+        with sr.Microphone() as source:
+            recognizer.adjust_for_ambient_noise(source)
             try:
-                # Use default microphone as source
-                with sr.Microphone() as source:
-                    print("Listening for commands")
-                    recognizer.adjust_for_ambient_noise(source)
-                    audio = recognizer.listen(source, timeout=LISTEN_TIMEOUT)
-                # Using Google for online recognition
-                recognized_audio = recognizer.recognize_google(audio)
-                print("You said: ", recognized_audio)
-                # Check for recognized commands and confirm before executing
-                executed = False
-                for command, phrases in COMMAND_PHRASES.items():
-                    for phrase in phrases:
-                        if phrase.lower() in recognized_audio.lower():
-                            execute_command(command)
-                            executed = True
-                            speak("Waiting for the next command")
-                            break
-                    if executed == True:
-                        break
-                if executed == False:
-                    speak("Command not executed.")
-                print("done?")
+                start_command = recognizer.listen(source, timeout=LISTEN_TIMEOUT)
+                recognized_start = recognizer.recognize_google(start_command).lower()
+                print(recognized_start)
+                # Check activation command
+                for phrase in WAKE_UP_PHRASES:
+                    if phrase.lower() in recognized_start:
+                        self.speak("What do you want me to do sir?")
+                        self.is_awake = True
             except sr.WaitTimeoutError as e:
-                print("No speech detected, waiting for a command")
+                # Timeout ocurred, continue waiting for wake-up phrase
+                pass
             except sr.UnknownValueError as e:
-                print("Could not understand: ", e)
+                # No recognized speech, continue waiting for wake-up phrase
+                pass
             except sr.RequestError as e:
-                print("Service Error: ", e)
+                # Error with the speech recognition service, continue waiting for the wake-up phrase
+                self.speak(e)
 
-def execute_command(command):
-    if command == "open discord":
-        open_discord()
-    elif command == "open browser":
-        open_browser()
-    elif command == "open visual studio code":
-        open_visual_studio()
-    elif command == "enter the matrix":
-        open_terminal()
-    elif command == "get date":
-        get_date()
-    elif command == "get time":
-        get_time()
-    elif command == "google this":
-        google_this()
-    elif command == "execute order 66":
-        order_66()
-    elif command == "exit the matrix":
-        exit_matrix()
+    def listen_for_commands(self):
+        is_listening = False
+        # Run the app continuosly
+        while True:
+            if not is_listening:
+                is_listening = self.jarvis_wake_up()
+                # if KILL_PHRASE.lower() in recognized_start.lower():
+                #   speak("I believe your intentions to be hostile")
+                #   return 0
+            elif is_listening:
+                try:
+                    # Use default microphone as source
+                    with sr.Microphone() as source:
+                        self.speak("Listening for commands")
+                        recognizer.adjust_for_ambient_noise(source)
+                        audio = recognizer.listen(source, timeout=LISTEN_TIMEOUT)
+                    # Using Google for online recognition
+                    recognized_audio = recognizer.recognize_google(audio)
+                    print("You said: ", recognized_audio)
+                    # Check for recognized commands and confirm before executing
+                    executed = False
+                    for command, phrases in COMMAND_PHRASES.items():
+                        for phrase in phrases:
+                            if phrase.lower() in recognized_audio.lower():
+                                self.execute_command(command)
+                                executed = True
+                                self.speak("Waiting for the next command")
+                                break
+                        if executed == True:
+                            break
+                    if executed == False:
+                        self.speak("Command not executed.")
+                    print("done?")
+                except sr.WaitTimeoutError as e:
+                    print("No speech detected, waiting for a command")
+                except sr.UnknownValueError as e:
+                    print("Could not understand: ", e)
+                except sr.RequestError as e:
+                    print("Service Error: ", e)
 
-def open_browser():
-    speak("Opening default web browser")
-    webbrowser.open_new_tab("http://") # Empty tab opening default user's homepage
-    print("opened browser")
+    def execute_command(self, command):
+        if command == "open discord":
+            self.open_discord()
+        elif command == "open browser":
+            self.open_browser()
+        elif command == "open visual studio code":
+            self.open_visual_studio()
+        elif command == "enter the matrix":
+            self.open_terminal()
+        elif command == "get date":
+            self.get_date()
+        elif command == "get time":
+            self.get_time()
+        elif command == "google this":
+            self.google_this()
+        elif command == "execute order 66":
+            self.order_66()
+        elif command == "exit the matrix":
+            self.exit_matrix()
 
-def google_this(query):
-    speak(f"Googling {query}")
-    search_url = (f"https://www.google.com/search?={query}")
-    webbrowser.open(search_url)
+    def open_browser(self):
+        self.speak("Opening default web browser")
+        webbrowser.open_new_tab("http://") # Empty tab opening default user's homepage
+        print("opened browser")
 
-def open_discord():
-    speak("Opening Discord")
-    subprocess.Popen("Discord", shell=True)
+    def google_this(self, query):
+        self.speak(f"Googling {query}")
+        search_url = (f"https://www.google.com/search?={query}")
+        webbrowser.open(search_url)
 
-def open_terminal():
-    speak("Connecting to the MATRIX")
-    command = "xterm -fullscreen -e cmatrix"
-    subprocess.Popen(command, shell=True)
+    def open_discord(self):
+        self.speak("Opening Discord")
+        subprocess.Popen("Discord", shell=True)
 
-def open_visual_studio():
-    speak("Opening Visual Studio Code")
-    os.system("code")
+    def open_terminal(self):
+        self.speak("Connecting to the MATRIX")
+        command = "xterm -fullscreen -e cmatrix"
+        subprocess.Popen(command, shell=True)
 
-def get_date():
-    current_date = datetime.date.today()
-    speak(format_date(current_date))
+    def open_visual_studio(self):
+        self.speak("Opening Visual Studio Code")
+        os.system("code")
 
-def get_time():
-    speak("The time is now")
+    def get_date(self):
+        current_date = datetime.date.today()
+        formatted_date = format_date(current_date)
+        self.speak(f"La fecha es: {formatted_date}")
 
-def order_66():
-    speak("You were the chosen one! It was said that you would destroy the Sith, not join them! Bring balance to the Force, not leave it in darkness!")
-    sys.exit()
+    def get_time(self):
+        self.speak("The time is now")
 
-def exit_matrix():
-    # Get the PID of the cmatrix process using pgrep
-    process = subprocess.Popen(["pgrep", "cmatrix"], stdout=subprocess.PIPE, text=True)
-    pid, _ = process.communicate()
-    # Terminate the cmatrix process using kill
-    if pid.strip().isdigit():
-        subprocess.run(["kill", pid.strip()])
-    speak("You have returned to Zion my child")
+    def order_66(self):
+        self.speak("You were the chosen one! It was said that you would destroy the Sith, not join them! Bring balance to the Force, not leave it in darkness!")
+        sys.exit()
 
+    def exit_matrix(self):
+        # Get the PID of the cmatrix process using pgrep
+        process = subprocess.Popen(["pgrep", "cmatrix"], stdout=subprocess.PIPE, text=True)
+        pid, _ = process.communicate()
+        # Terminate the cmatrix process using kill
+        if pid.strip().isdigit():
+            subprocess.run(["kill", pid.strip()])
+        self.speak("You have returned to Zion my child")
 
-def main():
-    listen_for_commands()
 
 if __name__ == "__main__":
-    main()
+    assistant = Jarvis()
+    while True:
+        if not assistant.is_awake:
+            assistant.jarvis_wake_up()
+        else:
+            assistant.listen_for_commands()
+            
 
